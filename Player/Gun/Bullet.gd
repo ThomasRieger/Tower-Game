6a87dd48105bbc0e9cd462ab2b6e975401fcb4a3
ThomasamohT <thomasrieger32@gmail.com>
2024-10-09 16:19:07 +0700

    add killable enemy

--- Player/Gun/Bullet.gd
+++ Player/Gun/Bullet.gd
@@ -1,14 +1,13 @@
 extends Area2D
 
 @export var speed = 200
-var hit = 0
+var collision = false
 
 func _ready() -> void:
 	top_level = true
 
 func _process(delta: float) -> void:
 	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
-	Vector2(1,0)
 	
 @warning_ignore("unused_parameter")
 func _physics_process(delta):
@@ -18,7 +17,12 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
-	if !body.name == "Player":
-		if body.name == "Enemy":
-			body.take_damage(1)
-		queue_free()
+	if !collision:
+		if !body.name == "Player":
+			if body.name.begins_with("Enemy"):
+				body.take_damage(1)
+			$Splash.emitting = true
+			collision = true
+			$Sprite2D.visible = false
+	else:
+		collision = false
